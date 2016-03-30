4fcce7e8056c12f9735b3b14ff675ad5a99d026b
Randall Britten <randall.britten@gmail.com>
2016-03-31 02:05:23 +1300

    Extracted keypad

--- src/PiRote/PiRote.hs
+++ src/PiRote/PiRote.hs
@@ -2,17 +2,21 @@
 module PiRote.PiRote where
 
 import Reflex.Dom
+
 digits :: [Integer]
 digits = [0..9]
 
 main :: IO ()
 main = mainWidget $ el "div" $ do
+  clickHistory <- keypad
+  dynText clickHistory
+
+keypad :: MonadWidget Spider m => m (Dynamic Spider String)
+keypad = do
   let bs = map (ble . show) digits
   bs' <- mapM id bs
   let bsm = mergeWith (++) bs'
-  clickHistory <- foldDyn (flip (++)) "" bsm
-  dynText clickHistory
-
+  foldDyn (flip (++)) "" bsm
 
 ble :: MonadWidget Spider m => String -> m (Event Spider String)
 ble label = button label >>= (return . fmap (const label) )
