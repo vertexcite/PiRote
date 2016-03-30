b71936ddbfdf757619311b2bad9b01aea4b4cabb
Randall Britten <randall.britten@gmail.com>
2016-03-31 01:56:36 +1300

    Remove code that was from demo

--- src/PiRote/PiRote.hs
+++ src/PiRote/PiRote.hs
@@ -7,13 +7,11 @@ digits = [0..9]
 
 main :: IO ()
 main = mainWidget $ el "div" $ do
-  t <- textInput def
   let bs = map (ble . show) digits
   bs' <- mapM id bs
   let bsm = mergeWith (++) bs'
   clickHistory <- foldDyn (flip (++)) "" bsm
   dynText clickHistory
-  dynText $ _textInput_value t
 
 
 ble :: MonadWidget Spider m => String -> m (Event Spider String)
