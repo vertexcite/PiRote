c9fc5d928445ce579c97e025bf126e0dbcb7e595
Randall Britten <randall.britten@gmail.com>
2016-04-09 12:10:24 +1200

    Keypad more reusable by returning events
    Client can choose what to do with events, in this case
    accumulating them in a long string.

--- src/PiRote/PiRote.hs
+++ src/PiRote/PiRote.hs
@@ -5,8 +5,9 @@ import Reflex.Dom
 
 main :: IO ()
 main = mainWidget $ el "div" $ do
-  clickHistory :: Dynamic Spider String <- keypad
-  dynText clickHistory
+  clickHistory :: Event Spider String <- keypad
+  enteredDigits :: Dynamic Spider String <- foldDyn (flip (++)) "" clickHistory
+  dynText enteredDigits
 
 keypadRow :: MonadWidget Spider m => [Integer] -> m (Event Spider String)
 keypadRow digits = do
@@ -19,7 +20,7 @@ keypadRow digits = do
     bsm = mergeWith (++) bs'
   return bsm
 
-keypad :: MonadWidget Spider m => m (Dynamic Spider String)
+keypad :: MonadWidget Spider m => m (Event Spider String)
 keypad = do
   kr1 :: Event Spider String <- keypadRow [7..9]
   kr2 :: Event Spider String <- keypadRow [4..6]
@@ -28,7 +29,7 @@ keypad = do
   let
     bsm :: Event Spider String
     bsm = mergeWith (++) [kr1, kr2, kr3, kr4]
-  foldDyn (flip (++)) "" bsm
+  return bsm
 
 ble :: MonadWidget Spider m => String -> m (Event Spider String)
 ble label = button label >>= (return . fmap (const label) )
