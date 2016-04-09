4a2cc75765c7656fdbc3d9a735a3f98e58ff4b8f
Randall Britten <randall.britten@gmail.com>
2016-04-09 12:47:08 +1200

    Rudimentary working keypad input

--- src/PiRote/PiRote.hs
+++ src/PiRote/PiRote.hs
@@ -27,7 +27,7 @@ main =
 
           t <- el "div" $ el "ul" $ do
             el "li" $ text "Enter memorised digits here (but look lower for assessment): "
-            el "li" $ piInput headstartDyn
+            el "li" $ piInput
 
           compareResultDyn <- combineDyn compareForPi headstartDyn t
           elAttr "div" ("class" =: "main") $ el "ul" $ do
@@ -74,18 +74,32 @@ numberInput = do
                                   Nothing -> errorState) result
   return result
 
-piInput :: (MonadWidget t m) => Dynamic t Int -> m (Dynamic t String)
-piInput headstartDyn = do
-  let errorState = Map.singleton "style" "border-color: red"
-      validState = Map.singleton "style" "border-color: green"
-  rec n <- textInput $ def & textInputConfig_attributes .~ attrs
-      countCorrectDyn <- combineDyn countCorrect headstartDyn (_textInput_value n)
-      inputLengthDyn <- mapDyn length (_textInput_value n)
-      countErrorsCurrentDyn <- combineDyn (-) inputLengthDyn countCorrectDyn
-      result <- mapDyn id $ _textInput_value n
-      attrs <- mapDyn (\r -> if r == 0 then validState else errorState) countErrorsCurrentDyn
-  return result
+piInput :: (MonadWidget t m) => m (Dynamic t String)
+piInput = do
+  clickHistory <- keypad
+  foldDyn (flip (++)) "" clickHistory
 
+keypadRow :: MonadWidget t m => [Integer] -> m (Event t String)
+keypadRow digits = do
+  let
+    bs = map (ble . show) digits
+  bs' <- mapM id bs
+  let
+    bsm = mergeWith (++) bs'
+  return bsm
+
+keypad :: MonadWidget t m => m (Event t String)
+keypad = do
+  kr1 <- el "div" $ keypadRow [7..9]
+  kr2 <- el "div" $ keypadRow [4..6]
+  kr3 <- el "div" $ keypadRow [1..3]
+  kr4 <- el "div" $ keypadRow [0]
+  let
+    bsm = mergeWith (++) [kr1, kr2, kr3, kr4]
+  return bsm
+
+ble :: MonadWidget t m => String -> m (Event t String)
+ble label = button label >>= (return . fmap (const label) )
 
 piString :: String
 piString = "3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912983367336244065664308602139494639522473719070217986094370277053921717629317675238467481846766940513200056812714526356082778577134275778960917363717872146844090122495343014654958537105079227968925892354201995611212902196086403441815981362977477130996051870721134999999837297804995105973173281609631859502445945534690830264252230825334468503526193118817101000313783875288658753320838142061717766914730359825349042875546873115956286388235378759375195778185778053217122680661300192787661119590921642019"
